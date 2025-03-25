7226e808bbd727bee997aba2275fbbb6d52a4bfc
Krists Krievins <krists.krievins@gmail.com>
2025-03-25 12:31:54 +0100

    Cards & Details Page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,3 +1,26 @@
+import { useState, useEffect } from "react";
+import BatchCard from "../components/BatchCard";
+import axios from "axios";
+
 export const LandingPage = () => {
-  return <div>Hello World!</div>;
+  const [batches, setBatches] = useState([]);
+  const [userId, setUserId] = useState(1);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5005/batches/?userId=${userId}`)
+      .then((res) => setBatches(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      Hello World!
+      <div>
+        {batches.map((oneBatch) => (
+          <BatchCard batchData={oneBatch} />
+        ))}
+      </div>
+    </div>
+  );
 };
