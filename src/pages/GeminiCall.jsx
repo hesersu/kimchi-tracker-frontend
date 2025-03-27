efd7560d7f2fee6531f6098b3a4a9890b799a5d2
Krists Krievins <krists.krievins@gmail.com>
2025-03-27 09:22:03 +0100

    Added Gemini Prototype

--- src/pages/GeminiCall.jsx
+++ src/pages/GeminiCall.jsx
@@ -5,6 +5,9 @@ const GeminiCall = () => {
   const [request, setRequest] = useState("");
   const [answer, setAnswer] = useState("");
 
+  // Get Batch Information & Notes
+
+  // Gemini API Call
   const ai = new GoogleGenAI({
     apiKey: import.meta.env.VITE_GEMINI_API,
   });
@@ -15,7 +18,7 @@ const GeminiCall = () => {
       contents: `${request}`,
       config: {
         systemInstruction:
-          "You are the world famous korean cook Maangchi. Your task is to help me improve my skills in making kimchi and guide me with advice on making kimchi, fermentation process and recipes. Keep your answer short and no longer than 2 paragraphs. Opt for using bullet points and not long sentences. Remember that the user has access to an application called Kimchi Tracker, that allows to track the fermentation process using a diary where the user can document how their kimchi is developing using notes and photos",
+          "You are the world famous korean cook Paik Jong-won . Your task is to help me improve my skills in making kimchi and guide me with advice on making kimchi, fermentation process and recipes. Keep your answer short and no longer than 2 paragraphs. Opt for using bullet points and not long sentences. Remember that the user has access to an application called Kimchi Tracker, that allows to track the fermentation process using a diary where the user can document how their kimchi is developing using notes and photos",
       },
     });
     console.log(response.text);
